9d7c92d3fd56b39fbb5851139daa4abab1ebb4e8
Mitesh <Mitesh21jun>
2025-04-29 15:24:30 +0530

    changed api to server

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'http://3.110.165.127:5000/api' });
 
 export const createTrade = (data) => API.post('/trade', data);
 export const getTrades = () => API.get('/trade');
